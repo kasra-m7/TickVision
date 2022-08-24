2b504cf570c6db32635b5513612807242f113e10
Eiman_Mahooti <eiman.mahooti007@gmail.com>
2022-08-24 07:56:01 +0430

    create layout table view and date view

--- app/src/main/java/ir/mahchegroup/vision/MainActivity.java
+++ app/src/main/java/ir/mahchegroup/vision/MainActivity.java
@@ -8,27 +8,36 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Gravity;
-import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
-import android.widget.ToggleButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-import com.github.clans.fab.FloatingActionMenu;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
-    FloatingActionMenu menu;
-    DrawerLayout drawer;
-    NavigationView navigation;
-    Toolbar toolbar;
+import ir.mahchegroup.vision.data_time.ChangeDate;
+import ir.mahchegroup.vision.data_time.ShamsiCalendar;
 
-    @SuppressLint("RtlHardcoded")
+public class MainActivity extends AppCompatActivity {
+    //    FloatingActionMenu menu;
+    private DrawerLayout drawer;
+    private NavigationView navigation;
+    private Toolbar toolbar;
+    private LinearLayout dateLayout, tableLayout;
+    private LayoutInflater inflater;
+    private View dateView, tableView;
+    private TextView tvDay, tvDate, tvTime, tvTitleReceive, tvTitlePayment, tvTitleProfit, tvTitleLeftover;
+    private RelativeLayout viewsLayout;
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
@@ -37,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         if (item.getItemId() == id) {
             if (drawer.isDrawerOpen(Gravity.RIGHT)) {
                 drawer.closeDrawer(Gravity.RIGHT);
-            }else {
+            } else {
                 drawer.openDrawer(Gravity.RIGHT);
             }
         }
@@ -46,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @RequiresApi(api = Build.VERSION_CODES.P)
-    @SuppressLint("NonConstantResourceId")
+    @SuppressLint({"NonConstantResourceId", "InflateParams"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,22 +67,24 @@ public class MainActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        setDateView();
 
-
-        menu.getChildAt(0).setOnClickListener(view -> {
-            Toast.makeText(this, "add", Toast.LENGTH_SHORT).show();
-            menu.close(true);
-        });
-
-        menu.getChildAt(1).setOnClickListener(view -> {
-            Toast.makeText(this, "select", Toast.LENGTH_SHORT).show();
-            menu.close(true);
-        });
-
-        menu.getChildAt(2).setOnClickListener(view -> {
-            Toast.makeText(this, "list", Toast.LENGTH_SHORT).show();
-            menu.close(true);
-        });
+        setTableView();
+//
+//        menu.getChildAt(0).setOnClickListener(view -> {
+//            Toast.makeText(this, "add", Toast.LENGTH_SHORT).show();
+//            menu.close(true);
+//        });
+//
+//        menu.getChildAt(1).setOnClickListener(view -> {
+//            Toast.makeText(this, "select", Toast.LENGTH_SHORT).show();
+//            menu.close(true);
+//        });
+//
+//        menu.getChildAt(2).setOnClickListener(view -> {
+//            Toast.makeText(this, "list", Toast.LENGTH_SHORT).show();
+//            menu.close(true);
+//        });
 
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close);
@@ -81,11 +92,89 @@ public class MainActivity extends AppCompatActivity {
         toggle.syncState();
     }
 
+    @SuppressLint("InflateParams")
+    private void setTableView() {
+        tableView = new View(this);
+        tvTitleReceive = tableView.findViewById(R.id.tv_title_receive);
+        tvTitlePayment = tableView.findViewById(R.id.tv_title_payment);
+        tvTitleProfit = tableView.findViewById(R.id.tv_title_profit);
+        tvTitleLeftover = tableView.findViewById(R.id.tv_title_leftover);
+
+        tableView = inflater.inflate(R.layout.table_view, null);
+
+        tableLayout.addView(tableView);
+    }
+
+
+    private void setDataTime() {
+        new Handler().postDelayed(() -> {
+            tvDate.setText(FaNum.convert(ChangeDate.getCurrentDay() + " / " + ChangeDate.getCurrentMonth() + " / " + ChangeDate.getCurrentYear()));
+            tvTime.setText(FaNum.convert(ChangeDate.getCurrentTime()));
+            tvDay.setText(showDay());
+        }, 1000);
+    }
+
+
+    private String showDay() {
+        String result = "";
+        int day = ShamsiCalendar.dayOfWeek(ShamsiCalendar.shSysDate());
+        switch (day) {
+            case 2:
+                result = "دوشنبه";
+                break;
+
+            case 3:
+                result = "سه شنبه";
+                break;
+
+            case 4:
+                result = "چهار شنبه";
+                break;
+
+            case 5:
+                result = "پنج شنبه";
+                break;
+
+            case 6:
+                result = "جمعه";
+                break;
+
+            case 7:
+                result = "شنبه";
+                break;
+
+            case 1:
+                result = "یکشنبه";
+        }
+        return result;
+    }
+
+    @SuppressLint("InflateParams")
+    private void setDateView() {
+        dateView = new View(this);
+        tvDay = dateView.findViewById(R.id.tv_day);
+        tvDate = dateView.findViewById(R.id.tv_date);
+        tvTime = dateView.findViewById(R.id.tv_time);
+
+        dateView = inflater.inflate(R.layout.date_view, null);
+
+        dateLayout.addView(dateView);
+
+        setDataTime();
+    }
+
 
     private void init() {
-        menu = findViewById(R.id.menu);
+//        menu = findViewById(R.id.menu);
         drawer = findViewById(R.id.drawer);
         navigation = findViewById(R.id.navigation);
         toolbar = findViewById(R.id.toolbar);
+
+        viewsLayout = drawer.findViewById(R.id.views_layout);
+
+        dateLayout = viewsLayout.findViewById(R.id.date_layout);
+        tableLayout = viewsLayout.findViewById(R.id.table_layout);
+
+        inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 }
